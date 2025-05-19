95f24828dc584ac40609331697ae840472fee1a9
Leandro Batista <leandrobouwierphp@gmail.com>
2025-05-19 20:22:36 -0300

    finalizando estilização

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,6 @@
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
+
 import styles from './Post.module.css';
 
 export function Post() {
@@ -5,7 +8,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/leandrobouwier.png" />
+                    <Avatar src="https://github.com/diego3g.png" />
                     <div className={styles.authorInfo}>
                         <strong>Diego Fernandes</strong>
                         <span>Web Developer</span>
@@ -24,6 +27,24 @@ export function Post() {
                     <a href="">#nlw</a>{' '}
                     <a href="">#rocketseat</a>
                 </p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea
+                    placeholder="Deixe um comentário"
+                />
+
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
+            </form>
+
+            <div className={styles.commentList}>
+                <Comment />
+                <Comment />
+                <Comment />
             </div>
         </article>
     )
