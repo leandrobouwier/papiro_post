8534497d0d9fd08ef7d7f927026ea42ce508987e
Leandro Batista <leandrobouwierphp@gmail.com>
2025-07-10 16:43:40 -0300

    like funcao

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,12 +1,19 @@
 import styles from './Comment.module.css';
-
+import { useState } from 'react';
 import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 
 export function Comment({ content, onDeleteComment }) {
 
+    const [likeCount, setLikeCount] = useState(0);
     function handleDeleteComment() {
         onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1;
+        });
     }
 
     return (
@@ -30,9 +37,9 @@ export function Comment({ content, onDeleteComment }) {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}> 
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
